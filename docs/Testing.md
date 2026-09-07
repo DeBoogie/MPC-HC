1954e622611b48e30828443ac2323671a7c79657
Eetu Tamminen <57353679+DeBoogie@users.noreply.github.com>
2026-09-08 02:28:24 +0300

    Escape dynamic web UI text

--- docs/Testing.md
+++ docs/Testing.md
@@ -86,6 +86,7 @@ On at least Intel, AMD and NVIDIA hardware where available:
 - Built-in controls-page status polling through `/status.json`.
 - Media/window titles containing quotes, backslashes, ampersands, angle brackets and non-ASCII text.
 - Dynamic title/status text must render literally rather than becoming HTML markup.
+- File-browser directory/file names and displayed paths containing ampersands or other HTML-significant text must render literally.
 - The legacy `/status.html` endpoint remains reachable for third-party compatibility.
 
 ### Web server CGI
--- src/mpc-hc/WebClientSocket.cpp
+++ src/mpc-hc/WebClientSocket.cpp
@@ -531,8 +531,7 @@ bool CWebClientSocket::OnIndex(CStringA& hdr, CStringA& body, CStringA& mime)
         const wmcmd& wc = s.wmcmds.GetNext(pos);
         CStringA str;
         str.Format("%u", wc.cmd);
-        CStringA valueName(UTF8(wc.GetName()));
-        valueName.Replace("&", "&amp;");
+        CStringA valueName = HtmlSpecialChars(UTF8(wc.GetName()));
         wmcoptions += "<option value=\"" + str + "\">" + valueName + "</option>\r\n";
     }
 
@@ -683,7 +682,7 @@ bool CWebClientSocket::OnBrowser(CStringA& hdr, CStringA& body, CStringA& mime)
 
                 files += "<tr>\r\n";
                 files +=
-                    "<td class=\"dirname\"><a href=\"[path]?path=" + UTF8Arg(fullpath) + "\">" + UTF8(fd.cFileName) + "</a></td>\r\n"
+                    "<td class=\"dirname\"><a href=\"[path]?path=" + UTF8Arg(fullpath) + "\">" + HtmlSpecialChars(UTF8(fd.cFileName)) + "</a></td>\r\n"
                     "<td class=\"dirtype\">Directory</td>\r\n"
                     "<td class=\"dirsize\">&nbsp;</td>\r\n"
                     "<td class=\"dirdate\"><span class=\"nobr\">" + CStringA(CTime(fd.ftLastWriteTime).Format(_T("%Y.%m.%d %H:%M"))) + "</span></td>\r\n";
@@ -718,8 +717,8 @@ bool CWebClientSocket::OnBrowser(CStringA& hdr, CStringA& body, CStringA& mime)
                     files += "<tr class=\"noext\">\r\n";
                 }
                 files +=
-                    "<td><a href=\"[path]?path=" + UTF8Arg(fullpath) + "\">" + UTF8(fd.cFileName) + "</a></td>\r\n"
-                    "<td><span class=\"nobr\">" + UTF8(type) + "</span></td>\r\n"
+                    "<td><a href=\"[path]?path=" + UTF8Arg(fullpath) + "\">" + HtmlSpecialChars(UTF8(fd.cFileName)) + "</a></td>\r\n"
+                    "<td><span class=\"nobr\">" + HtmlSpecialChars(UTF8(type)) + "</span></td>\r\n"
                     "<td><span class=\"nobr\">" + size + "</span></td>\r\n"
                     "<td><span class=\"nobr\">" + CStringA(CTime(fd.ftLastWriteTime).Format(_T("%Y.%m.%d %H:%M"))) + "</span></td>\r\n";
                 files += "</tr>\r\n";
@@ -774,9 +773,9 @@ bool CWebClientSocket::OnControls(CStringA& hdr, CStringA& body, CStringA& mime)
 
     m_pWebServer->LoadPage(IDR_HTML_CONTROLS, body, AToT(m_path));
     body.Replace("[filepatharg]", UTF8Arg(path));
-    body.Replace("[filepath]", UTF8(path));
+    body.Replace("[filepath]", HtmlSpecialChars(UTF8(path)));
     body.Replace("[filedirarg]", UTF8Arg(dir));
-    body.Replace("[filedir]", UTF8(dir));
+    body.Replace("[filedir]", HtmlSpecialChars(UTF8(dir)));
     body.Replace("[state]", UTF8(state));
     body.Replace("[statestring]", UTF8(statestring));
     body.Replace("[position]", UTF8(NumToCString(std::lround(m_pMainFrame->GetPos() / 10000i64))));
